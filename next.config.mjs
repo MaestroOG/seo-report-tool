38caa6fe4458e5f2c346de5e6c327224628e3e00
MaestroOG <muneeburrehman822@gmail.com>
2026-01-31 10:47:41 +0500

    trying out playwright-core now (please work)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  serverExternalPackages: ['@sparticuz/chromium'],
+  serverComponentsExternalPackages: ['playwright-core', '@sparticuz/chromium-min'],
   reactCompiler: true,
 };
 
